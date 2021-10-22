5823a44574a06d8e8f7bc97d29ad8be5816b9fdd
Barno_CODE <thebrunosmail@gmail.com>
2021-10-22 18:09:15 -0300

    comiteo lo ultimo que hicimos,

--- src/database/queries.js
+++ src/database/queries.js
@@ -9,13 +9,26 @@ exports.getStatusMessages = () => {
   });
 };
 
+const channelTypes = {
+  welcome: "welcome_channel",
+  exit: "exit_channel"
+}
 
 exports.getwelcomeServerID = (serverID) => {
-  let welcomeServer = executeQuery(
-    `SELECT config_value FROM bot_configuration WHERE server_id = '${serverID}' AND parameter = 'WELCOME_SERVER'`
+  return getMessageChannelID(serverID, channelTypes.welcome);  
+};
+
+exports.getexitServerID = (serverID) => {
+  return getMessageChannelID(serverID, channelTypes.exit);  
+};
+
+function getMessageChannelID(serverID, channelType){
+
+  let channel = executeQuery(
+    `SELECT '${channelType}' FROM servers WHERE server_id = '${serverID}'`
   );
   return new Promise((resolve, reject) => {
-    welcomeServer.then((result) => {
+    channel.then((result) => {
       let response = null;
       if (result.rows.length > 0) {
         response = result.rows[0].config_value;
@@ -23,4 +36,5 @@ exports.getwelcomeServerID = (serverID) => {
       resolve(response);
     });
   });
+
 };
--- src/manager.js
+++ src/manager.js
@@ -1,26 +1,58 @@
-const queries = require('./database/queries')
-const Discord = require('discord.js')
+const queries = require("./database/queries");
+const Discord = require("discord.js");
 
 exports.manageMemberAdd = (client, member) => {
+  let welcomeServerID = queries.getwelcomeServerID(member.guild.id);
 
-    let welcomeServerID = queries.getwelcomeServerID(member.guild.id)
-    welcomeServerID.then((welcomeChannel) => {
-        if (welcomeChannel!=null) {
-            let server = client.guilds.cache.get(member.guild.id)
-            server.channels.fetch().then((channels) => {
+  welcomeServerID.then((exitChannel) => {
+    let channel = getChannelByID(client, member.guild.id, exitChannel);
 
-                let channel = channels.get(welcomeChannel)
-                if (channel!= undefined) {
-                 //   let message = new Discord.MessageEmbed()
-                    let message = {
-                        title: "WELCOME",
-                        description: "¡ <@256475475566657537> ha llegado a #ClubDorito! ¿Traerá comida? Bienvenid@, disfruta del servidor. Puedes ir a #🔰丨roles para elegir a qué quieres tener acceso ^^",
-                        image: {url: "https://media.discordapp.net/attachments/884892638426308669/896400692359606352/Doritos_en_espacio.png?width=910&height=682"}
-                        
-                    }
-                    channel.send({ embeds:[message]})
-                }
-            })
-        }
-    })
+    if (channel != undefined) {
+      channel.then((chn) => {
+        let message = {
+          title: "WELCOME",
+          description: `¡ <@${member.id}> ha llegado a #ClubDorito! ¿Traerá comida? Bienvenid@, disfruta del servidor. Puedes ir a #🔰丨roles para elegir a qué quieres tener acceso ^^`,
+          image: {
+            url: "https://media.discordapp.net/attachments/884892638426308669/896400692359606352/Doritos_en_espacio.png?width=910&height=682",
+          },
+        };
+        chn.send({ embeds: [message] });
+      });
+    }
+  });
+};
+
+exports.manageMemberdelete = (client, member) => {
+  let exitServerID = queries.getexitServerID(member.guild.id);
+
+  exitServerID.then((exitChannel) => {
+    let channel = getChannelByID(client, member.guild.id, exitChannel);
+
+    if (channel != undefined) {
+      channel.then((chn) => {
+        let message = {
+          title: "GU BAI",
+          description: `¡ <${member.user.username}> se fue :c!`,
+          image: {
+            url: "https://media.discordapp.net/attachments/884892638426308669/896400692359606352/Doritos_en_espacio.png?width=910&height=682",
+          },
+        };
+        chn.send({ embeds: [message] });
+      });
+    }
+  });
+};
+
+function getChannelByID(client, guild_id, channel_id) {
+  if (channel_id != null) {
+    return new Promise((resolve, reject) => {
+      let server = client.guilds.cache.get(guild_id);
+      server.channels
+        .fetch()
+        .then((channels) => {
+          resolve(channels.get(channel_id));
+        })
+        .catch((err) => reject(err));
+    });
+  }
 }
