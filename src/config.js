6cc3b4ab59d53f88706cbd7dac7d380c3cfa5c3a
Barno_CODE <thebrunosmail@gmail.com>
2021-10-24 13:36:37 -0300

    Add cheesecake-test-bot

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    BOT: {
-        token: process.env.BOT_TOKEN,
-        prefix: process.env.BOT_PREFIX,
-        owner: process.env.BOT_OWNER,
-        database: process.env.DATABASE_URL
-    }
-}
+  BOT: {
+    token: process.env.BOT_TOKEN,
+    prefix: process.env.BOT_PREFIX,
+    owner: process.env.BOT_OWNER,
+    database: process.env.DATABASE_URL,
+  }
+};
